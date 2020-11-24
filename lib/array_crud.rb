0af920b1f566c26433cefaeadbc0cfdc44a8ba02
Jeffrey Platt <jsplatt@gmail.com>
2020-11-24 16:30:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,5 +46,5 @@ end
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number = 4
-  retrieve_element_from_index(array, 4, "learning")
+  retrieve_element_from_index(array[4])
 end
